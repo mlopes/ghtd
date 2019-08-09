d08077eb87c0df1209a99182073b8b1988e5cd4a
Marco Lopes <marco.paulo.lopes@gmail.com>
2019-08-09 07:26:01 +0100

    Add action to add

--- src/Command.hs
+++ src/Command.hs
@@ -9,7 +9,7 @@ import Lib
 import qualified Options.Applicative as O
 
 data Command
-  = Add
+  = Add String
   | Remove
   | Projects
   | Contexts
@@ -25,17 +25,25 @@ resolveCommand = O.execParser parserInfo
 
 parserInfo :: O.ParserInfo (Maybe Command)
 parserInfo = info' parser' "This is the main prog desc"
-  where
-    parser' :: O.Parser (Maybe Command)
-    parser' =
-      O.optional $
-      (O.subparser . F.foldMap command')
-        [ ("add", "Add new action", pure Add)
-        , ("rm", "Remove an action", pure Remove)
-        , ("projects", "Show projects", pure Projects)
-        , ("contexts", "Show contexts", pure Contexts)
-        , ("help", "Help I'm being repressed!", pure Help)
-        ]
-    info' :: O.Parser a -> String -> O.ParserInfo a
-    info' p desc = O.info (O.helper <*> p) (O.fullDesc <> O.progDesc desc)
-    command' (cmdName, desc, parser) = O.command cmdName (info' parser desc)
+
+parser' :: O.Parser (Maybe Command)
+parser' =
+  O.optional $
+  (O.subparser . F.foldMap command)
+    [ ("add", "Add new action", Add <$> addParams)
+    , ("rm", "Remove an action", pure Remove)
+    , ("projects", "Show projects", pure Projects)
+    , ("contexts", "Show contexts", pure Contexts)
+    , ("help", "Help I'm being repressed!", pure Help)
+    ]
+
+info' :: O.Parser a -> String -> O.ParserInfo a
+info' p desc = O.info (O.helper <*> p) (O.fullDesc <> O.progDesc desc)
+
+command (cmdName, desc, parser) = O.command cmdName (info' parser desc)
+
+addParams :: O.Parser String
+addParams = O.argument O.str (O.metavar "ACTION")
+-- Saving this here as a reference on how to parse a bunch of optional positional arguments
+-- addParams :: O.Parser (Maybe [String])
+-- addParams = O.optional $ O.some (O.argument O.str (O.metavar "FILTER..."))
